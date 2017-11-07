dce3844f2a4bb653a9b573577ed2b54123229b03
MAM34158 <MAM34158@creighton.edu>
2017-11-06 20:56:59 -0600

    Added CSS

--- PHP/loginpage.php
+++ PHP/loginpage.php
@@ -3,7 +3,7 @@
 	<title>Remote Access Tool</title>
 		<style type="text/css">
 		</style>
-		<link href="HTML/styleMain.css" rel="stylesheet" type="text/css">
+		<link href="../HTML/styleMain.css" rel="stylesheet" type="text/css">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
