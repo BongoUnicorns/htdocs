7f971f50395220d5ca95e0d89c14534ef5b782a3
jackische <jackische@gmail.com>
2017-11-06 20:38:16 -0600

    Fixed form submission for username and password index.html and loginpage.php

--- PHP/loginpage.php
+++ PHP/loginpage.php
@@ -11,8 +11,8 @@
 <h1><a href='RegistrationPage.php' class='ResizingTitle'>LoginPage</a></h1></Flagtext>
 
 <div class='BodyTextBox'><div class='BodyTitle'></div><div class='mainTextHeader'>This is a PHP script to handle inputs from the index.</div><div class='mainText'>
-	<?php echo $_POST["username"]; ?><br><br>
-	<?php echo $_POST["password"]; ?><br><br>
+	Welcome <?php echo $_POST["username"]; ?><br><br>
+	Your password is: <?php echo $_POST["password"]; ?><br><br>
 
 
 
