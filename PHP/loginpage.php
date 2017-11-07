a96cfb3364ae07116101647d074da4951cf21659
MAM34158 <MAM34158@creighton.edu>
2017-11-06 20:34:44 -0600

    Handling the form pass from index revised

--- PHP/loginpage.php
+++ PHP/loginpage.php
@@ -1,25 +1,9 @@
 <html>
-<head>
-<title>CSC 551 Home</title>
-<style type='text/css'>
-	</style>
-<link href='../HTML/styleMain.css' rel='stylesheet' type='text/css'>
-<meta name='viewport' content='width=device-width, initial-scale=1.0'>
-</head>
 <body>
-<Flagtext class='Flag'><titleimg style='float:left'><a href='index.html'><img src="../Images/Logo.png" style='width:40px;height:auto'1;></img></a></titleimg>
-<h1><a href='RegistrationPage.php' class='ResizingTitle'>LoginPage</a></h1></Flagtext>
-
-<div class='BodyTextBox'><div class='BodyTitle'></div><div class='mainTextHeader'>This is a PHP script to handle inputs from the index.</div><div class='mainText'>
 	<?php echo $_POST["username"]; ?><br><br>
 	<?php echo $_POST["password"]; ?><br><br>
 
 
-
-
-
-
-</div></div>
 
 </body>
 </html>
