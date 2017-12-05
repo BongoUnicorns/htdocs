3a20c49666a5555129067a01ebca5fbcd98b6cbc
jackische <jackische@gmail.com>
2017-12-04 23:51:57 -0600

    even MORE characters added to the chararray for the token generation

--- PHP/loginpage.php
+++ PHP/loginpage.php
@@ -82,7 +82,7 @@ elseif(isset($_POST["add"]) && $_POST["usernamesub"] != ""){
 	$usrstring = strtolower($usrstring);
 	$hash = strlen($usrstring);
 
-	$chararray = array(' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_');
+	$chararray = array(' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '?', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
 	$firstchar=substr($usrstring,0,1);
 	$lastchar=substr($usrstring,$hashint-1,1);
 
